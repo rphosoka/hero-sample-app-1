8dbc2089a06b6f71174e316d35a8c54aa764dfd0
わんちゃん <k.hosokawa@mht-code.com>
2020-04-08 19:33:48 +0900

    init this project

---
+++ HeroSampleApp1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HeroSampleApp1
+//
+//  Created by 細川和也 on 2020/04/08.
+//  Copyright © 2020 wannoko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
